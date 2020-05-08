f52f5da8ab38cd4d9d32ff4d43fbc1d2f307d3f9
Romain Tello <romain.tello@gmail.com>
2020-05-08 12:54:20 +0200

    Move home page to /blog

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,28 +2,9 @@
 	Script
 -->
 <script context="module">
-	import { queryDatoCms } from '../utils';
-	import { ArticlePreview } from '../components/article';
-
-	export const preload = async ({ params, query }) =>
-		queryDatoCms(`
-			query HomeQuery {
-				allArticles(orderBy: _createdAt_DESC) {
-					_createdAt
-					content(markdown: true)
-					cover {
-						url
-					}
-					slug
-					title
-				}
-			}
-		`)
-	;
 </script>
 
 <script>
-	export let data;
 </script>
 
 
@@ -31,11 +12,6 @@
 	Style
 -->
 <style>
-	.grid {
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-		gap: 2em;
-	}
 </style>
 
 
@@ -47,9 +23,3 @@
 </svelte:head>
 
 <h1>My beautiful blog</h1>
-
-<section class="grid">
-	{#each data.allArticles as article}
-		<ArticlePreview {...article} />
-	{/each}
-</section>
